d0ce5b461fab6d2c63f199895675907d376834d5
LuisVZ485 <juggerlavz23@gmail.com>
2021-12-28 16:52:33 -0500

    variable

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-const nombre = document.getElementById("name")
+'use strict';
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const passw = document.getElementById("password");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
